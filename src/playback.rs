cec51f828f7fbb0885612e9657ef2ecfa3f1d345
skittle <afshaanlondhe4@gmail.com>
2023-02-02 15:05:37 +0530

    playback metronome

--- src/playback.rs
+++ src/playback.rs
@@ -1,41 +1,90 @@
-/*
-get the audiomanager handle,
-accept a samplebank
-accept a sequence
-play them
+/**
+This module manages the playback thread
 */
-
 use kira::manager::{backend::cpal::CpalBackend, AudioManager, AudioManagerSettings};
-use std::{error::Error, time::Duration};
+use kira::ClockSpeed;
+use std::{error::Error, sync::mpsc::Receiver};
 
-use crate::{sequencer::SampleSequence, ActiveSamples};
+use crate::{sequencer::SampleSequence, sequencer::Sequence};
 
 pub struct PlayBack {
     audio_manager: AudioManager,
+    mute: bool,
+    playing_sequence: SampleSequence,
 }
 
 impl PlayBack {
     /// set up audio playback
     pub fn setup() -> Result<PlayBack, Box<dyn Error>> {
         let m = AudioManager::<CpalBackend>::new(AudioManagerSettings::default())?;
-        Ok(PlayBack { audio_manager: m })
+        Ok(PlayBack {
+            audio_manager: m,
+            mute: true,
+            playing_sequence: SampleSequence::new(0, 0),
+        })
     }
 }
 
-// fn to update the sequence or a sample
-// fn play_with: take sequence and sample bank then begin playing
-// fn pause: stop
-// fn play: start
-
 pub trait Player {
-    fn play_with(&mut self, sequence: SampleSequence, samples: ActiveSamples) -> ();
+    /// this is the playback loop
+    fn begin_playback(&mut self, control_channel: Receiver<SampleSequence>) -> ();
 }
 
 impl Player for PlayBack {
-    fn play_with(&mut self, sequence: SampleSequence, sample_bank: ActiveSamples) -> () {
-        self.audio_manager.play(sample_bank.samples[0].clone());
-        std::thread::sleep(Duration::from_secs(2));
-        self.audio_manager.play(sample_bank.samples[1].clone());
-        std::thread::sleep(Duration::from_secs(2));
+    fn begin_playback(&mut self, control_channel: Receiver<SampleSequence>) -> () {
+        self.mute = false;
+        let mut step = 0;
+        let ticks_per_step = 1;
+
+        let metronome = self
+            .audio_manager
+            .add_clock(ClockSpeed::TicksPerMinute(100.0))
+            .unwrap();
+
+        metronome.start().unwrap();
+
+        let mut time_last_loop = metronome.time();
+        let mut ticks_elapsed: u64 = 0;
+
+        loop {
+            if self.mute {
+                continue;
+            }
+
+            // check for control messages and handle them
+            match control_channel.try_recv() {
+                Ok(seq) => {
+                    self.playing_sequence = seq;
+                    step = 0;
+                    // TODO: if update is a change dont reset step count
+                }
+                Err(_) => (),
+            };
+
+            let time_now = metronome.time();
+            ticks_elapsed += time_now.ticks - time_last_loop.ticks;
+            time_last_loop.ticks = time_now.ticks;
+
+            if ticks_elapsed >= ticks_per_step {
+                for t in self.playing_sequence.tracks() {
+                    if t[step] {
+                        print!("+");
+                    } else {
+                        print!("_");
+                    }
+                }
+                println!("");
+
+                if step == self.playing_sequence.steps() - 1 {
+                    println!("\n");
+                }
+                // increment index
+                step = (step + 1) % self.playing_sequence.steps();
+
+                ticks_elapsed = 0;
+            }
+
+            // if something fucks up return break and return an err
+        }
     }
 }
