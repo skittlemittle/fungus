a30b93358efa1214469fb884d79a3f3f078cd247
skittle <afshaanlondhe4@gmail.com>
2023-02-16 10:24:18 +0530

    dw about this

--- src/samples.rs
+++ src/samples.rs
@@ -8,9 +8,9 @@ pub type ActiveSamples = Vec<StaticSoundData>;
 pub fn load() -> Result<ActiveSamples, Box<dyn Error>> {
     let mut samples: Vec<StaticSoundData> = vec![];
 
-    for i in 1..3 {
+    for i in 1..4 {
         let sample =
-            StaticSoundData::from_file(format!("{i}.ogg"), StaticSoundSettings::default())?;
+            StaticSoundData::from_file(format!("{i}.wav"), StaticSoundSettings::default())?;
         samples.push(sample);
     }
     Ok(samples as ActiveSamples)
