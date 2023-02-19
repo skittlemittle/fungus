0d1f222633bdd0948880f79c68c73ee01570539d
skittle <afshaanlondhe4@gmail.com>
2023-02-19 13:24:38 +0530

    handle input, move control out of main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,123 @@
+use std::error::Error;
+use std::sync::mpsc;
+use std::thread;
+
+use pancurses::Input;
+
 pub mod playback;
 pub mod samples;
 pub mod sequencer;
 pub mod test_ui;
+
+use kira::sound::static_sound::StaticSoundData;
+use playback::{Controls, PlayBack, Player};
+use sequencer::{SampleSequence, Sequence};
+use test_ui::{Display, ScrContent};
+
+/// app? state :trollface:
+struct State {
+    pub tempo: u32,
+    pub muted: bool,
+    pub selected_track: usize,
+    pub sequence: SampleSequence,
+    pub samples: Vec<StaticSoundData>,
+}
+
+/// The program control loop
+///
+/// display: handle to curses display
+pub fn play(display: Display) -> Result<(), Box<dyn Error>> {
+    let samples = samples::load()?;
+
+    let mut state = State {
+        muted: false,
+        tempo: 100,
+        selected_track: 0,
+        sequence: SampleSequence::new(samples.len(), 8),
+        samples,
+    };
+
+    let (seq_tx, seq_rx) = mpsc::channel();
+    let (control_tx, control_rx) = mpsc::channel();
+
+    let mut player = PlayBack::setup(state.samples)?;
+
+    let playback_handle = thread::spawn(move || {
+        // FIXME: unwrap lmao
+        player.begin_playback(seq_rx, control_rx).unwrap();
+    });
+
+    seq_tx.send(state.sequence.get_sequence())?;
+
+    loop {
+        // read inputs
+        let command = match Display::getch(&display) {
+            Some(Input::Character(c)) => c,
+            Some(_) => '0',
+            None => '0',
+        };
+
+        let mut should_update = false;
+        match command {
+            'j' => {
+                if state.selected_track >= 1 {
+                    state.selected_track -= 1;
+                }
+            }
+            'k' => {
+                if state.selected_track < state.sequence.num_tracks() - 1 {
+                    state.selected_track += 1;
+                }
+            }
+            'm' => {
+                state.muted = !state.muted;
+                should_update = true;
+            }
+            ' ' => {
+                let c_step = 1;
+                state
+                    .sequence
+                    .set_step(state.selected_track, c_step, true)?;
+                should_update = true;
+            }
+            'c' => {
+                let c_step = 1;
+                state
+                    .sequence
+                    .set_step(state.selected_track, c_step, false)?;
+                should_update = true;
+            }
+            'z' => {
+                state.sequence.clear_track(state.selected_track);
+                should_update = true;
+            }
+            _ => {}
+        }
+
+        if !should_update {
+            continue;
+        }
+
+        control_tx.send(Controls {
+            tempo: state.tempo,
+            mute: state.muted,
+        })?;
+
+        seq_tx.send(state.sequence.get_sequence())?;
+
+        Display::update(
+            &display,
+            ScrContent {
+                muted: state.muted,
+                tempo: state.tempo,
+                play: true,
+                step: 1,
+                sequence: &state.sequence,
+            },
+        )
+    }
+
+    playback_handle.join().unwrap();
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,11 @@
-use std::error::Error;
-use std::sync::mpsc;
-use std::thread;
+use fungus::test_ui::Display;
 
-use fungus::test_ui::{Display, ScrContent};
 fn main() {
     let disp = Display::begin();
 
-    match play(disp) {
+    match fungus::play(disp) {
         Err(e) => println!("{}", e),
         Ok(()) => (),
     }
     Display::end();
-}
-
-use fungus::playback::{Controls, PlayBack, Player};
-use fungus::samples;
-use fungus::sequencer::{SampleSequence, Sequence};
-
-fn play(display: Display) -> Result<(), Box<dyn Error>> {
-    let mut seq = SampleSequence::new(3, 5);
-
-    seq.set_step(0, 0, true)?;
-    seq.set_step(0, 4, true)?;
-
-    seq.set_step(1, 3, true)?;
-    seq.set_step(1, 0, true)?;
-
-    seq.set_step(2, 1, true)?;
-    seq.set_step(2, 3, true)?;
-
-    let samples = samples::load()?;
-
-    let mut player = PlayBack::setup(samples)?;
-
-    let (seq_tx, seq_rx) = mpsc::channel();
-    let (control_tx, control_rx) = mpsc::channel();
-
-    let handle = thread::spawn(move || {
-        // TODO: show (and log) error and restart playback
-        player.begin_playback(seq_rx, control_rx).unwrap();
-    });
-
-    Display::update(
-        &display,
-        ScrContent {
-            muted: false,
-            tempo: 180,
-            play: true,
-            step: 3,
-            sequence: &seq,
-        },
-    );
-
-    seq_tx.send(seq.get_sequence())?;
-
-    std::thread::sleep(std::time::Duration::from_secs(2));
-    control_tx.send(Controls {
-        tempo: 130,
-        mute: true,
-    })?;
-    Display::update(
-        &display,
-        ScrContent {
-            muted: true,
-            tempo: 130,
-            play: true,
-            step: 3,
-            sequence: &seq,
-        },
-    );
-
-    std::thread::sleep(std::time::Duration::from_secs(4));
-    control_tx.send(Controls {
-        tempo: 130,
-        mute: false,
-    })?;
-
-    seq.set_step(0, 0, false)?;
-    seq_tx.send(seq.get_sequence())?;
-
-    Display::update(
-        &display,
-        ScrContent {
-            muted: false,
-            tempo: 130,
-            play: true,
-            step: 3,
-            sequence: &seq,
-        },
-    );
-
-    handle.join().unwrap();
-
-    Ok(())
 }
